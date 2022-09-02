7aed65882ae290852f59959c15dc76ebb9b08687
VishnuPrasadhSR <vishnuprasath1610@gmail.com>
2022-09-02 10:11:42 +0530

    initial commit

---
+++ precache-manifest.983d5b5c4e89a4260340978ca3460438.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8029e32d38a55d46b31d81e7e1a4ae56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c03192c34d9171afa8b1",
+    "url": "/static/css/main.f5f3cc52.chunk.css"
+  },
+  {
+    "revision": "2947ec934e1515b6d40a",
+    "url": "/static/js/2.f9afb314.chunk.js"
+  },
+  {
+    "revision": "0c91d6cf6408089b81f706ebde51ebe6",
+    "url": "/static/js/2.f9afb314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03192c34d9171afa8b1",
+    "url": "/static/js/main.bebde00b.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "f2bfc2e55c030954c5aa5f84c42b5f8b",
+    "url": "/static/media/logo.f2bfc2e5.svg"
+  }
+]);
